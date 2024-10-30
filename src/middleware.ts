f96eb1b5487ab4bbf1fe2c764cb4521b7a6432f1
MrRex <souvikhazra151@gmail.com>
2024-10-30 20:22:44 +0530

    middleware updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
-export { default } from "next-auth/middleware";
 import { getToken } from "next-auth/jwt";
 
 export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
   const url = request.nextUrl;
+
   if (
     token &&
     (url.pathname.startsWith("/sign-in") ||
@@ -13,10 +13,22 @@ export async function middleware(request: NextRequest) {
   ) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
+
   if (!token && url.pathname.startsWith("/dashboard")) {
     return NextResponse.redirect(new URL("/sign-in", request.url));
   }
+
+  return NextResponse.next();
 }
+
 export const config = {
-  matcher: ["/", "/sign-in", "/sign-up", "/dashboard/:path*", "/verify/:path*"],
+  matcher: [
+    "/",
+    "/sign-in",
+    "/sign-up",
+    "/forgot-password",
+    "/reset-password",
+    "/dashboard/:path*",
+    "/verify/:path*",
+  ],
 };
